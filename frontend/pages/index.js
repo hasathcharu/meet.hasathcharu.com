3435dfabf7fdd903f02d4bbbe813c03623f68301
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2022-08-29 18:47:02 +0530

    meet.hasathcharu.com frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import 'tailwindcss/tailwind.css';
+import Hero from '../components/Hero';
+
+export default function Home() {
+    return (
+        <Hero/>
+    );
+}
