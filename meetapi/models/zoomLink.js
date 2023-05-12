2d2e8dd69181a401ba1e1a3b2dadbd2092a1a942
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2023-05-12 08:10:34 +0530

    conection

--- meetapi/models/zoomLink.js
+++ meetapi/models/zoomLink.js
@@ -44,12 +44,14 @@ module.exports = class ZoomLink {
       }
       if (result[0]?.affectedRows == 1) {
         await connection.commit();
+        if (connection) connection.release();
         return 'Success';
       }
-      await connection.rollback();
       if (result[0]?.affectedRows == 0) throw new Error('Link Not Found');
       throw new Error();
     } catch (err) {
+      await connection.rollback();
+      if (connection) connection.release();
       if (err.message == 'Link Not Found') return 'Link Not Found';
       return 'Fail';
     }
